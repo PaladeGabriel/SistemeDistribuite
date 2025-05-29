59703f776e65b7e0f6b71ca86d88fca2d17b6258
PaladeGabriel <127981229+PaladeGabriel@users.noreply.github.com>
2025-05-29 14:46:42 +0300

    Lab5Ex

---
+++ lab5Lib/src/main/kotlin/com/sd/laborator/interfaces/HTMLPrinter.kt
@@ -0,0 +1,5 @@
+package com.sd.laborator.interfaces
+import com.sd.laborator.model.Book
+interface HTMLPrinter {
+    fun printHTML(books: Set<Book>): String
+}
---
+++ lab5Lib/src/main/kotlin/com/sd/laborator/interfaces/JSONPrinter.kt
@@ -0,0 +1,5 @@
+package com.sd.laborator.interfaces
+import com.sd.laborator.model.Book
+interface JSONPrinter {
+    fun printJSON(books: Set<Book>): String
+}
